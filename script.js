c939c5e706c2c27b3765c56e34188248c1ba3b19
x0bt <123811196+x0bt@users.noreply.github.com>
2023-04-27 16:11:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let tg = window.Telegram.WebApp;
 print(tg.initData());
-tg.expand();
+
+let main_page = document.querySelector('#main_page');
+
+if  (main_page){
+    window.Telegram.WebApp.expand()
+    window.Telegram.WebApp.MainButton.onClick(buttonOn)
+    window.Telegram.WebApp.MainButton.setParams({'text': 'Корзина'})
+    window.Telegram.WebApp.MainButton.show()
+}
